f21f2934e27bb7347bce5a3682c4a008769c7a24
Artyom <art.laureau@gmail.com>
2019-06-25 18:08:54 +0200

    Test folder created
    First step: Updating deprecated functions ( tf.contrib)

--- tests/Resmodule.py
+++ tests/Resmodule.py
@@ -26,3 +26,61 @@ def factorized_res_module(x, is_training, dropout=0.3, dilation=[1,1], l2=None, 
     print("DEBUG: {} {}".format(name, y.shape.as_list()))
     print("DEBUG: L2 in factorized res module {}".format(l2))
     return y
+    
+class non_bottleneck_1d (nn.Module):
+    def __init__(self, chann, dropprob, dilated):        
+        super().__init__()
+
+        
+        self.conv3x1_1 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(1,0), bias=True)
+        self.conv1x3_1 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1), bias=True)
+
+        self.conv3x1_2 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(1*dilated,0), bias=True, dilation = (dilated,1))
+        self.conv1x3_2 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1*dilated), bias=True, dilation = (1, dilated))
+
+        self.bn1 = nn.BatchNorm2d(chann, eps=1e-03)
+        self.bn2 = nn.BatchNorm2d(chann, eps=1e-03)
+
+        self.dropout = nn.Dropout2d(dropprob)
+
+#https://www.tensorflow.org/api_docs/python/tf/nn/atrous_conv2d
+tf.nn.atrous_conv2d(
+    value,
+    filters,
+    rate,
+    padding,
+    name=None
+)
+
+tf.nn.conv2d(
+    input,
+    filter=None,
+    strides=None,
+    padding=None,
+    use_cudnn_on_gpu=True,
+    data_format='NHWC',
+    dilations=[1, 1, 1, 1],
+    name=None,
+    filters=None
+)
+tf.contrib.layers.conv2d(
+    inputs,
+    num_outputs,
+    kernel_size,
+    stride=1,
+    padding='SAME',
+    data_format=None,
+    rate=1,
+    activation_fn=tf.nn.relu,
+    normalizer_fn=None,
+    normalizer_params=None,
+    weights_initializer=initializers.xavier_initializer(),
+    weights_regularizer=None,
+    biases_initializer=tf.zeros_initializer(),
+    biases_regularizer=None,
+    reuse=None,
+    variables_collections=None,
+    outputs_collections=None,
+    trainable=True,
+    scope=None
+)
